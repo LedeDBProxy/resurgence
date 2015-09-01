625d9638e04988e34ffcb0e94da701a8f9fdf704
lazio579 <wangbin5790@163.com>
2015-09-01 15:54:29 +0800

    Fix backend state problems

--- lib/proxy/balance.lua
+++ lib/proxy/balance.lua
@@ -25,12 +25,14 @@ function idle_failsafe_rw()
 	local backend_ndx = 0
 
 	for i = 1, #proxy.global.backends do
-		local s = proxy.global.backends[i]
-        if s.type == proxy.BACKEND_TYPE_RW and s.state ~= proxy.BACKEND_STATE_DOWN then
-            local conns = s.pool.users[proxy.connection.client.username]
-            if conns.cur_idle_connections > 0 then
-                backend_ndx = i
-                break
+        local s = proxy.global.backends[i]
+        if s.type == proxy.BACKEND_TYPE_RW then
+            if s.state == proxy.BACKEND_STATE_UP or s.state == proxy.BACKEND_STATE_UNKNOWN then
+                local conns = s.pool.users[proxy.connection.client.username]
+                if conns.cur_idle_connections > 0 then
+                    backend_ndx = i
+                    break
+                end
             end
         end
 	end
@@ -44,13 +46,15 @@ function idle_ro()
 
 	for i = 1, #proxy.global.backends do
 		local s = proxy.global.backends[i]
-        if s.type == proxy.BACKEND_TYPE_RO and s.state ~= proxy.BACKEND_STATE_DOWN then
-            local conns = s.pool.users[proxy.connection.client.username]
-            -- pick a slave which has some idling connections
-            if conns.cur_idle_connections > 0 then
-                if max_conns == -1 or s.connected_clients < max_conns then
-                    max_conns = s.connected_clients
-                    max_conns_ndx = i
+        if s.type == proxy.BACKEND_TYPE_RO then
+            if s.state == proxy.BACKEND_STATE_UP or s.state == proxy.BACKEND_STATE_UNKNOWN then
+                local conns = s.pool.users[proxy.connection.client.username]
+                -- pick a slave which has some idling connections
+                if conns.cur_idle_connections > 0 then
+                    if max_conns == -1 or s.connected_clients < max_conns then
+                        max_conns = s.connected_clients
+                        max_conns_ndx = i
+                    end
                 end
             end
         end
